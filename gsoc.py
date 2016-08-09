7d663dfe8fdef4270195fc8223d098c73784c63b
dpshelio <dps.helio@gmail.com>
2016-08-09 14:14:10 +0100

    updated code with a faster histogram

--- gsoc.py
+++ gsoc.py
@@ -1,15 +1,28 @@
-import sunpy
 import sunpy.map
 import matplotlib.pyplot as plt
+from skimage import exposure, img_as_float
 
-aia = sunpy.map.Map('/home/sudarshan/AIA20110319_105400_0171.fits')
-aia.plot(vmin=100, vmax=4000)
-plt.colorbar()
-plt.draw()
-##raw_input()
+def plot_map_histogram(vmin=None, vmax=None):
+    aia = sunpy.map.Map(AIA171)
+    if not vmin:
+        vmin = aia.data.min()
+    if not vmax:
+        vmax = aia.data.max()
+    plt.figure(figsize=(15,5))
+    ax1 = plt.subplot(1, 2, 1)
+    aia.plot(vmin=vmin, vmax=vmax)
+    plt.colorbar()
 
-plt.figure()
-plt.hist(aia.data, bins=100)
-plt.draw()
+    ax2 = plt.subplot(1, 2, 2)
+    y,x = exposure.histogram(aia.data, nbins=100)
+    ax2.plot(x, y)
+    ax2.set_xrange=[aia.min() - 1000, aia.max() + 1000]
+    ax2.axvline(vmin, color='r')
+    ax2.axvline(vmax, color='g')
 
-plt.show()
+
+if __name__ == '__main__':
+    plot_map_histogram()
+    plot_map_histogram(vmin=0, vmax=100)
+    plot_map_histogram(vmin=500, vmax=5000)
+    plt.show()
